cf19e4854abd8e70e53fd998d1b70726ea2d5517
Paulo Tech <112776520+d-guimaraes-llc@users.noreply.github.com>
2022-10-11 12:09:29 -0500

    Feature/add obs2 workspace config (#1)
    * Added config.obs for making a better obs env
    
    Removed symlinked config.ron from repo
    
    Partial update of switch-config.sh to support scripted switching to obs config.
    
    * Added config.obs for 2WorkSpace obs config
    
    Synced settings between configs
    
    Co-authored-by: Daniel Guimaraes <dguimaraesllc@protonmail.com>

--- scripts/switch-config.sh
+++ scripts/switch-config.sh
@@ -1,34 +1,76 @@
 #!/bin/bash
 
+# Some static data
 FULLSCREEN="config.fullscreen.ron"
 HALFSCREEN="config.halfscreen.ron"
+SCEENS="config.2screen.ron"
 FILEPATH="$HOME/.config/leftwm" 
 FILE="config.ron"
 
-# use /usr/bin/ls incase ls is aliased
+# options to be set by params
+obs=false
+verbose=false
+
+# Parsing params with getopt
+options=$(getopt -l "help,obs,vebose" -o "hov" -a -- "$@")
+
+eval set -- "$options"
+while true
+do
+    case "$1" in
+        -h|--help)
+            showHelp
+            exit 0
+            ;;
+        -o|--obs)
+            obs=true
+            ;;
+        -v|--verbose)
+            verbose=true
+            ;;
+        --)
+            shift
+            break;;
+    esac
+    shift
+done
+
+print_str () {
+    if $verbose; then echo "$1"; fi
+}
+
+print_str "Getting current config file"
 current=$(/usr/bin/ls -l "$FILEPATH/$FILE" | awk '{print $11}')
-echo "The current is: $current"
+print_str "Current is set to: $current"
 
-if [[ "$current" =~ .*"$FULLSCREEN" ]]; then
-    echo "Switching to halfscreen"
-    unlink "$FILEPATH/$FILE"
-    ln -s "$FILEPATH/$HALFSCREEN" "$FILEPATH/$FILE"
-    echo "Setting resolution to 1920x1080"
-    xrandr --output DisplayPort-1 --mode 1920x1080 
-    echo "Setting layout to MainAndVertStack"
-    leftwm-command "SetLayout MainAndVertStack"
-else
-    echo "Swiching to fullscreen"
-    unlink "$FILEPATH/$FILE"
-    ln -s "$FILEPATH/$FULLSCREEN" "$FILEPATH/$FILE"
-    echo "Setting resolution to 3840x1080"
-    xrandr --output DisplayPort-1 --mode 3840x1080
-    echo "Setting layout to CenterMainFluid"
-    leftwm-command "SetLayout CenterMainFluid"
-fi
 
-echo "Preforming a soft reload now."
-leftwm command SoftReload 
+exit 0
+
+default_switch () {
+    if [[ "$current" =~ .*"$FULLSCREEN" ]]; then
+        print_str "Switching to halfscreen"
+        unlink "$FILEPATH/$FILE"
+        ln -s "$FILEPATH/$HALFSCREEN" "$FILEPATH/$FILE"
+        if verbose; then echo "Setting resolution to 1920x1080"; fi
+        xrandr --output DisplayPort-1 --mode 1920x1080 
+        if verbose; then echo "Setting layout to MainAndVertStack"; fi
+        leftwm-command "SetLayout MainAndVertStack"
+    else
+        if verbose; then echo "Swiching to fullscreen"; fi
+        unlink "$FILEPATH/$FILE"
+        ln -s "$FILEPATH/$FULLSCREEN" "$FILEPATH/$FILE"
+        if verbose; then echo "Setting resolution to 3840x1080"; fi
+        xrandr --output DisplayPort-1 --mode 3840x1080
+        if verbose; then echo "Setting layout to CenterMainFluid"; fi
+        leftwm-command "SetLayout CenterMainFluid"
+    fi
+}
+
+relaod () {
+    echo "Preforming a soft reload now."
+    leftwm command SoftReload 
+}
+
 
 new=$(ls -l "$FILEPATH/$FILE" | awk '{print $11}')
 echo "The new config file is $new"
