d4a3a3df669e5d9adeae4c73c0265fe4464bd6f4
Daniel Guimaraes <dguimaraesllc@protonmail.com>
2022-10-14 09:21:16 -0500

    Updated the script to sync several sections
    Updated many corresponding functions to support this.
    
    Added a fullmatch re to match the closing, this will allow for subsection copies in the future

--- scripts/keybind_sync.py
+++ scripts/keybind_sync.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 
-import string
-import subprocess
+
 import os
 import re
+import subprocess
 from collections import deque
 from pprint import pprint as pp
 
-FILEPATH = os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir, os.pardir))
+FILEPATH = os.path.abspath(
+    os.path.join(os.path.realpath(__file__), os.pardir, os.pardir)
+)
 FILE = "config.ron"
 NUM_BACKUPS = 5
 BACKUPS_DIR = os.path.join(FILEPATH, "config_backups")
+SYNC_SECTIONS = ["keybind", "tags", "window_rules"]
 
-print(FILEPATH, FILE)
 
 def get_current_config_file() -> str:
     command1 = ["/usr/bin/ls", "-l", os.path.join(FILEPATH, FILE)]
@@ -21,7 +23,9 @@ def get_current_config_file() -> str:
     if len(res) == 1:
         return res[0]
     else:
-        raise RuntimeError("Unable to get current config, check stdout warning messages")
+        raise RuntimeError(
+            "Unable to get current config, check stdout warning messages"
+        )
 
 
 def get_config_file_names() -> list:
@@ -40,40 +44,44 @@ def load_config_file(file_name: str) -> str:
         return file.readlines()
 
 
-def load_configs(all_config_files: list) -> dict:
+def load_configs() -> dict:
     all_configs = {}
+    all_config_files = get_config_file_names()
     for config_file in all_config_files:
         config = load_config_file(config_file)
         all_configs[config_file] = config
     return all_configs
 
 
-def extract_keybinds_from_current(config_dict: dict, current: str) -> list:
-    keybinds = []
+def extract_section_from_current(config_dict: dict, current: str, section: str) -> list:
+    data = []
     for config_name, config in config_dict.items():
         if config_name in current:
-            start_idx = find_start_idx(config, "keybind")
+            start_idx = find_start_idx(config, section)
             end_idx = find_end_idx(config, start_idx)
             for i in range(start_idx, end_idx):
-                keybinds.append(config[i])
-    return keybinds
+                data.append(config[i])
+    return data
 
 
-def update_keybinds_to_other_configs(config_dict: dict, keybinds: list, current: str):
+def update_section_to_other_configs(
+    config_dict: dict, data: list, current: str, section: str
+):
     for config_name, config in config_dict.items():
         if config_name not in current:
-            start_idx = find_start_idx(config, "keybind")
+            start_idx = find_start_idx(config, section)
             end_idx = find_end_idx(config, start_idx)
             new_config = []
             for i in range(0, start_idx):
                 new_config.append(config[i])
-            for item in keybinds:
+            for item in data:
                 new_config.append(item)
             for i in range(end_idx, len(config)):
                 new_config.append(config[i])
+            config_dict[config_name] = new_config
 
 
-def find_start_idx(config: list, section: string) -> int:
+def find_start_idx(config: list, section: str) -> int:
     for idx, line in enumerate(config):
         if section in line:
             return idx + 1  # start on next line
@@ -82,29 +90,33 @@ def find_start_idx(config: list, section: string) -> int:
 def find_end_idx(config: list, idx: int):
     while True:
         idx += 1
-        if config[idx].endswith("],\n"):
+        if re.fullmatch(r"^\s+],\n$", config[idx]):
             return idx
 
 
-def validate_configs(config_dict: dict, current: str, section: str) -> tuple:
+def validate_configs(config_dict: dict, current: str) -> tuple:
     valid = False
     errors = []
     current_config = load_config_file(current)
-    for config_name, config in config_dict.items():
-        if config_name not in current:
-            current_config_idx = find_start_idx(current_config, section)
-            to_validate_config_idx = find_start_idx(config, section)
-            while current_config_idx < len(current_config):
-                if current_config[current_config_idx] != config[to_validate_config_idx]:
-                    errors.append({
-                        "line": to_validate_config_idx,
-                        "config": config_name,
-                    })
-                    break
-                current_config_idx += 1
-                to_validate_config_idx += 1
-    if len(errors) == 0:
-        valid = True
+    for section in SYNC_SECTIONS:
+        for config_name, config in config_dict.items():
+            if config_name not in current:
+                current_config_idx = find_start_idx(current_config, section)
+                end_config_idx = find_end_idx(current_config, current_config_idx)
+                to_validate_config_idx = find_start_idx(config, section)
+                while current_config_idx < end_config_idx:
+                    if current_config[current_config_idx] != config[to_validate_config_idx]:
+                        errors.append(
+                            {
+                                "line": to_validate_config_idx,
+                                "config": config_name,
+                            }
+                        )
+                        break
+                    current_config_idx += 1
+                    to_validate_config_idx += 1
+        if len(errors) == 0:
+            valid = True
     return (valid, errors)
 
 
@@ -124,8 +136,14 @@ def delete_oldest_backup(backups: deque):
     os.remove(os.path.join(BACKUPS_DIR, backup.split(" ")[-1]))
 
 
+def create_backups_dir():
+    if not os.path.exists(BACKUPS_DIR):
+        os.mkdir(BACKUPS_DIR)
+
+
 def backup_old_configs(config_dict: list, current: str) -> bool:
     success = False
+    create_backups_dir()
     for config_name, config in config_dict.items():
         if config_name not in current:
             command1 = ["ls", "-ltr", BACKUPS_DIR]
@@ -138,25 +156,38 @@ def backup_old_configs(config_dict: list, current: str) -> bool:
             filename = f"{date}_{config_name}"
             with open(os.path.join(BACKUPS_DIR, filename), "w") as file:
                 file.writelines(config)
+            success = True
+            if not success:
+                break
     return success
 
 
 def write_new_configs(config_dict: dict, current: str):
+    print("Writing new configs")
     for config_name, config in config_dict.items():
+        file_name = os.path.join(FILEPATH, config_name)
+        print(f"New config file: {file_name}")
         if config_name not in current:
-            with open(os.path.join(FILEPATH, config_name), "w") as file:
-                file.writelines(config)
+            with open(file_name, "w") as file:
+                file.write("".join(config))
 
 
 if __name__ == "__main__":
-    current_config_file = get_current_config_file()
-    all_config_files = get_config_file_names()
-    config_dict = load_configs(all_config_files)
-    keybinds = extract_keybinds_from_current(config_dict, current_config_file)
-    backup_old_configs(config_dict, current_config_file)
-    update_keybinds_to_other_configs(config_dict, keybinds, current_config_file)
-    (valid, errors) = validate_configs(config_dict, current_config_file, "keybind")
-    if valid:
-        write_new_configs(config_dict, current_config_file)
-    else:
-        pp(f"errors: {errors}")
+    current_config_file_name = get_current_config_file()
+    config_dict = load_configs()
+    if backup_old_configs(config_dict, current_config_file_name):
+        print("Backup successful")
+        for section in SYNC_SECTIONS:
+            data = extract_section_from_current(
+                config_dict, current_config_file_name, section
+            )
+            update_section_to_other_configs(
+                config_dict, data, current_config_file_name, section
+            )
+        (valid, errors) = validate_configs(
+            config_dict, current_config_file_name
+        )
+        if valid:
+            write_new_configs(config_dict, current_config_file_name)
+        else:
+            pp(f"errors: {errors}")
